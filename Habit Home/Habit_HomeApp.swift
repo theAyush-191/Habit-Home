5574910d92ccca6bd88516b9eaf2d48cefd0f442
theAyush-191 <ayush191.znr@gmail.com>
2025-09-16 20:37:08 +0530

    Initial Commit

---
+++ Habit Home/Habit_HomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Habit_HomeApp.swift
+//  Habit Home
+//
+//  Created by Ayush Singh on 16/09/25.
+//
+
+import SwiftUI
+
+@main
+struct Habit_HomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Habit HomeUITests/Habit_HomeUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  Habit_HomeUITestsLaunchTests.swift
+//  Habit HomeUITests
+//
+//  Created by Ayush Singh on 16/09/25.
+//
+
+import XCTest
+
+final class Habit_HomeUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
